0e552ad2e45acc56d3d624d53ffa4a71ffe97e98
Aditi <aditi.tsr@gmail.com>
2023-08-02 18:35:38 +0530

    Initial commit

---
+++ PerfectNum.java
@@ -0,0 +1,22 @@
+package ppt;
+
+public class PerfectNum {
+	static void perfect(int n) {
+		int sum=1;
+		if(n==1)
+			System.out.println("Not perfect");
+		for(int i=2;i<n;i++) {
+			if(n%i==0)
+				sum=sum+i;
+		}
+		if(sum==n)
+			System.out.println("Perfect");
+		else
+			System.out.println("Not Perfect");
+	}
+	public static void main(String[] args) {
+		int n=33550336;
+		perfect(n);
+	}
+
+}
---
+++ Union.java
@@ -0,0 +1,20 @@
+package ppt;
+import java.util.*;
+public class Union {
+	static void distinct(int a[], int b[]) {
+		HashSet<Integer> hs=new HashSet<>();
+		for(int i=0;i<a.length;i++)
+			hs.add(a[i]);
+		for(int j=0;j<b.length;j++)
+			hs.add(b[j]);
+		System.out.println("Distinct elements in union of array is:"+hs.size());
+		System.out.println("Distinct elements:"+hs);
+	}
+	public static void main(String[] args) {
+		int a[]= {1,4,7,3,6,13,18};
+		int b[]= {2,6,5,9,16,21,4};
+		distinct(a,b);
+
+	}
+
+}
