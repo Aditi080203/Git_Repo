38fa3a2c0ae60b12f2e8b040471ce83a627edbe1
Aditi <aditi.tsr@gmail.com>
2023-08-04 23:13:16 +0530

    added

---
+++ calculator.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("Enter operator: ");
+            char ch = sc.next().trim().charAt(0);
+            if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+                System.out.println("Enter two operands:");
+                int n1 = sc.nextInt();
+                int n2 = sc.nextInt();
+                if (ch == '+')
+                    System.out.println(n1 + n2);
+                if (ch == '-')
+                    System.out.println(n1 - n2);
+                if (ch == '*')
+                    System.out.println(n1 * n2);
+                if (ch == '/')
+                    System.out.println(n1 / n2);
+            } else if (ch == 'x' || ch == 'X') {
+                break;
+            } else
+                System.out.println("not valid operator");
+        }
+        sc.close();
+    }
+}
