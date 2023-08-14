04d4d48bcded5a2c3fc5f9af9eefb895b8e58157
AditiSh <aditi.tsr@gmail.com>
2023-08-14 22:47:30 +0530

    Added

---
+++ Armstrong.java
@@ -0,0 +1,20 @@
+public class Armstrong {
+    static boolean isArmstrong(int n) {
+        int num = n;
+        int sum = 0;
+        while (n > 0) {
+            int rem = n % 10;
+            sum = sum + rem * rem * rem;
+            n = n / 10;
+        }
+        return num == sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isArmstrong(153));
+        for (int i = 100; i < 1000; i++) {
+            if (isArmstrong(i))
+                System.out.println(i);
+        }
+    }
+}
