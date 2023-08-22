7b5e7539d6ba783ad0f146bfa04d03425c38c6f6
AditiSh <aditi.tsr@gmail.com>
2023-08-22 23:29:58 +0530

    Added

---
+++ Basic/ColumnTitle.java
@@ -0,0 +1,18 @@
+package Basic;
+
+public class ColumnTitle {
+    static String convertTitle(int columnNumber) {
+        StringBuilder result = new StringBuilder();
+        while (columnNumber > 0) {
+            columnNumber--;
+            char c = (char) ('A' + columnNumber % 26);
+            result.insert(0, c);
+            columnNumber /= 26;
+        }
+        return result.toString();
+    }
+    public static void main(String[] args) {
+        int num = 701;
+        System.out.println(convertTitle(num));
+    }
+}
